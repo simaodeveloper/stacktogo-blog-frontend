ddc26e40bd69a9450181546ff91ceaf91e03db0e
Daniel Simão <simaodeveloper@gmail.com>
2019-01-08 09:40:58 -0200

    remove javascript behavior

--- src/.components/card/_card.babel.js
+++ src/.components/card/_card.babel.js
@@ -4,15 +4,3 @@
  * @param {HTMLElement} el - Component's main DOM element
  * @param {Object} params - Params to be used
  */
-
-const $image = $('.card__image', el);
-const bigImagePath = $image.attr('data-big-image');
-
-const $bigImage = $('<img />');
-
-$bigImage.on('load', () => {
-    $image.attr('src', $bigImage.attr('src'));
-    $image.removeClass('card__image--blur');
-});
-
-$bigImage.attr('src', bigImagePath);
